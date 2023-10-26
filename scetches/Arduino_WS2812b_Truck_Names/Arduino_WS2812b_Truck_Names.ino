f129ce0010fff21232d6ae883824c1b79cc799b8
Igor Peshkov <igor.peshkov@gmail.com>
2023-10-26 06:44:22 +0300

    Update main scetch

--- scetches/Arduino_WS2812b_Truck_Names/Arduino_WS2812b_Truck_Names.ino
+++ scetches/Arduino_WS2812b_Truck_Names/Arduino_WS2812b_Truck_Names.ino
@@ -9,9 +9,17 @@
 #define LED_MATRIX_1_PIN 2  // Pin to cotrol first ws812b leds
 #define LED_MATRIX_2_PIN 3  // Pin to cotrol second ws812b leds
 
-#define NUM_LEDS 256  // Count of leds
+#define NUM_LEDS 256     // Count of leds
+#define NUM_LED_COLS 8 // Count of leds in one row
+
+const uint8_t NUM_LED_ROWS = NUM_LEDS / NUM_LED_COLS; // Count of led rows 
 
 CRGB leds[NUM_LEDS];
+
+#define START_TOP_RIGHT 1
+#define START_TOP_LEFT 0
+
+#define LED_START_DIRECTION START_TOP_RIGHT // first point (led)
 
 /// @param scale a 0-255 value for how much to scale all leds before writing them out
 uint8_t current_brightness = 50;
@@ -24,10 +32,10 @@ byte bryghtness_vals[BRIGHTNESS_VALS_COUNT];
 byte bryghtness_val_index = 0; // calculate brightness count
 
 // color vars
-uint32_t collors[] = // uint32_t 0 .. 4,294,967,295
+uint32_t colors[] = // uint32_t 0 .. 4,294,967,295
 {
   CRGB::Red,
-  CRGB::Orange,
+  // CRGB::Orange,
   CRGB::Yellow,
   CRGB::Green,
   CRGB::Cyan,
@@ -35,9 +43,27 @@ uint32_t collors[] = // uint32_t 0 .. 4,294,967,295
   CRGB::Purple,
 };
 
-const byte collors_count = sizeof(collors) / sizeof(collors[0]);
+const byte colors_count = sizeof(colors) / sizeof(colors[0]);
 
 byte current_color; // byte 	0..256
+
+#define LETTER_HEIGHT 6
+
+const byte LET_Z[][LETTER_HEIGHT] = {{1, 0, 0, 0, 0, 1},{1, 1, 0, 0, 0, 1},{1, 0, 1, 0, 0, 1},{1, 0, 0, 1, 0, 1},{1, 0, 0, 0, 1, 1}};
+const byte LET_H[][LETTER_HEIGHT] = {{1, 1, 1, 1, 1, 1},{0, 0, 1, 0, 0, 0},{0, 0, 1, 0, 0, 0},{0, 0, 1, 0, 0, 0},{1, 1, 1, 1, 1, 1}};
+const byte LET_E[][LETTER_HEIGHT] = {{1, 0, 0, 0, 0, 1},{1, 0, 1, 0, 0, 1},{1, 0, 1, 0, 0, 1},{1, 1, 1, 1, 1, 1}};
+const byte LET_N[][LETTER_HEIGHT] = {{1, 1, 1, 1, 1, 1},{0, 0, 0, 0, 1, 0},{0, 0, 0, 1, 0, 0},{0, 0, 1, 0, 0, 0},{1, 1, 1, 1, 1, 1}};
+const byte LET_I[][LETTER_HEIGHT] = {{1, 1, 1, 1, 1, 1}};
+const byte LET_A[][LETTER_HEIGHT] = {{0, 0, 1, 1, 1, 1},{0, 1, 0, 1, 0, 0}, {1, 0, 0, 1, 0, 0},{0, 1, 0, 1, 0, 0},{0, 0, 1, 1, 1, 1}};
+const byte LET_P[][LETTER_HEIGHT] = {{0, 1, 1, 0, 0, 0},{1, 0, 0, 1, 0, 0}, {1, 0, 0, 1, 0, 0},{1, 1, 1, 1, 1, 1}};
+const byte LET_V[][LETTER_HEIGHT] = {{1, 1, 1, 1, 0, 0},{0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 1},{0, 0, 0, 0, 1, 0},{1, 1, 1, 1, 0, 0}};
+const byte LET_L[][LETTER_HEIGHT] = {{0, 0, 0, 0, 0, 1},{0, 0, 0, 0, 0, 1}, {0, 0, 0, 0, 0, 1},{1, 1, 1, 1, 1, 1}};
+
+#define ROWS_WRAP 1
+#define COLLS_WRAP 1
+#define LETTER_SPACE 1
+
+uint16_t current_position_x = ROWS_WRAP;
 
 // buttons vars
 #define BTN_CHANGE_COLOR 12 // pin to control matrix change color
@@ -48,6 +74,148 @@ bool btn_change_color_current_state = LOW;
 
 bool btn_switch_names_last_state = LOW;
 bool btn_switch_names_current_state = LOW;
+
+// animation
+bool animation_state = HIGH; // state that show animation in progress
+bool animation_mode_state = HIGH; // state HIGH show that animation start displaying. LOW - finished displaying
+
+uint16_t current_animation_position_x = 0;
+uint16_t current_animation_position_y = 0;
+int16_t current_animation_direction_x = 1;
+int16_t current_animation_direction_y = 0;
+
+unsigned long global_timer; // global timer
+unsigned long mode_timer; // timer for animations
+
+#define GLOBAL_LOOP 2 // global timer loop
+#define COLOR_MODE_SPEED 3 // animation speed
+
+// convert i, j coordinates to led num for ws2812b 
+int get_led_num(uint8_t i = 0, uint8_t j = 0){
+  switch (LED_START_DIRECTION) {
+    case START_TOP_RIGHT:
+      return NUM_LED_COLS * i + ((i % 2) ? j : (NUM_LED_COLS - j - 1));
+      break;
+    case START_TOP_LEFT:
+    default:
+      return 0;
+      break;
+  }
+}
+
+void drawChar(byte _symbol[][6], byte letter_width, uint16_t &current_position_x){
+  //byte letter_width = sizeof(_symbol) / sizeof(_symbol[0]);
+  
+  uint16_t _x = 0;
+  uint16_t _y = 0;
+  
+  // if (DEBUG_MODE == true) {
+  //   Serial.print("letter width: ");
+  //   Serial.println(sizeof(_symbol));
+  //   Serial.println(sizeof(LET_Z));
+
+  //   Serial.print("possition: ");
+  //   Serial.println(current_position_x);
+  // }
+
+  for (int j = 0; j < LETTER_HEIGHT; j++) {
+    for (int i = 0; i < letter_width; i++) {
+      if (_symbol[i][j] == 1) {
+        // rotate letter
+        _x = current_position_x + (letter_width - 1) - i;
+        _y = (LETTER_HEIGHT - 1) + COLLS_WRAP - j;
+
+        // leds[get_led_num(i, j)] = colors[current_color];
+        leds[get_led_num(_x, _y)] = colors[current_color];
+      }
+    }
+  }
+
+  current_position_x += letter_width;
+}
+
+void drawName(){
+  current_position_x = ROWS_WRAP;
+
+  FastLED.clear();
+
+  // ZHENIA
+  drawChar(LET_Z, sizeof(LET_Z) / sizeof(LET_Z[0]), current_position_x);
+  current_position_x += LETTER_SPACE;
+  drawChar(LET_H, sizeof(LET_H) / sizeof(LET_H[0]), current_position_x);
+  current_position_x += LETTER_SPACE;
+  drawChar(LET_E, sizeof(LET_E) / sizeof(LET_E[0]), current_position_x);
+  current_position_x += LETTER_SPACE;
+  drawChar(LET_N, sizeof(LET_N) / sizeof(LET_N[0]), current_position_x);
+  current_position_x += LETTER_SPACE;
+  drawChar(LET_I, sizeof(LET_I) / sizeof(LET_I[0]), current_position_x);
+  current_position_x += LETTER_SPACE;
+  drawChar(LET_A, sizeof(LET_A) / sizeof(LET_A[0]), current_position_x);
+
+  // PAVEL
+  // current_position_x = ROWS_WRAP + 2;
+
+  // drawChar(LET_P, sizeof(LET_P) / sizeof(LET_P[0]), current_position_x);
+  // current_position_x += LETTER_SPACE;
+  // drawChar(LET_A, sizeof(LET_A) / sizeof(LET_A[0]), current_position_x);
+  // current_position_x += LETTER_SPACE;
+  // drawChar(LET_V, sizeof(LET_V) / sizeof(LET_V[0]), current_position_x);
+  // current_position_x += LETTER_SPACE;
+  // drawChar(LET_E, sizeof(LET_E) / sizeof(LET_E[0]), current_position_x);
+  // current_position_x += LETTER_SPACE;
+  // drawChar(LET_L, sizeof(LET_L) / sizeof(LET_L[0]), current_position_x);
+
+  FastLED.show();
+}
+
+void animateSwitchOn() {
+  if (animation_mode_state) {
+    leds[get_led_num(current_animation_position_x, current_animation_position_y)] = colors[current_color];
+  } else {
+    leds[get_led_num(current_animation_position_x, current_animation_position_y)] = CRGB::Black;
+  }
+
+  current_animation_position_x += current_animation_direction_x;
+  current_animation_position_y += current_animation_direction_y;
+
+  if (current_animation_position_x >= NUM_LED_ROWS - 1) {
+    if (current_animation_direction_y == 0) {
+      current_animation_direction_y = 1;
+      current_animation_direction_x = 0;
+    }
+
+    if (current_animation_position_y >= NUM_LED_COLS - 1) {
+      current_animation_direction_y = 0;
+      current_animation_direction_x = -1;
+    }
+  } else if (current_animation_position_x == 0) {
+    if (current_animation_position_y >= NUM_LED_COLS - 1) {
+      current_animation_direction_y = -1;
+      current_animation_direction_x = 0;
+    }
+
+    if (current_animation_position_y == 0) {
+      current_animation_direction_y = 0;
+      current_animation_direction_x = 1;
+
+      // change mode 
+      if (!animation_mode_state) {
+        animation_state = LOW;
+        animation_mode_state = HIGH;
+
+        drawName();
+      } else {
+        animation_mode_state = LOW;
+      }
+    }
+  }
+}
+
+void changeCurrentColor(){
+  if (++current_color >= colors_count) {
+    current_color = 0;
+  }
+}
 
 // for debounce buttons
 bool debounce(uint8_t btn_pin, bool last) {
@@ -80,35 +248,10 @@ byte analogReadMedium(byte* bryghtness_vals_arr) {
     sum += bryghtness_vals_arr[i];
   }
 
-  Serial.print("sum: ");
-  Serial.print(sum);
-  Serial.println();
-
-  // Serial.print("sum / BRIGHTNESS_VALS_COUNT: ");
-  // Serial.print(((float)sum / (float)BRIGHTNESS_VALS_COUNT));
-  // Serial.println();
-  // Serial.print(round((float)sum / (float)BRIGHTNESS_VALS_COUNT));
-  // Serial.println();
-
-  
-
   // sum = sum>>4;
-
-  // Serial.print("sum>>4: ");
-  // Serial.print(sum);
-  // Serial.println();
   
   return round((float)sum / (float)BRIGHTNESS_VALS_COUNT);
   //return (int)((float)sum / (float)BRIGHTNESS_VALS_COUNT) + 1;  
-}
-
-void print_analog_vals(){
-  for (uint8_t i = 0 ; i < BRIGHTNESS_VALS_COUNT; i++ ) {
-    Serial.print(bryghtness_vals[i]);
-    Serial.print(" ");
-  }
-  
-  Serial.println();
 }
 
 // update device settings
@@ -128,7 +271,7 @@ void readEEPROM() {
   }
 
   // set random figure color
-  if (current_color >= collors_count) {
+  if (current_color >= colors_count) {
       current_color = 0;
   }
 }
@@ -150,8 +293,11 @@ void setup() {
 
   // make lead black before start
   FastLED.clear();
-  fill_solid(leds, 256, collors[current_color]);
-  FastLED.show();
+  //fill_solid(leds, 256, colors[current_color]);
+
+  //drawName();
+  
+  // FastLED.show();
 
   // setup potentiometer
   pinMode(POT_BRIGHTNESS_PIN, INPUT);
@@ -171,8 +317,6 @@ void loop() {
     
     if ( new_brightness != current_brightness ) {
       if (DEBUG_MODE) {
-        // print_analog_vals();
-
         Serial.print("New brightness: ");
         Serial.println(new_brightness);
       }
@@ -196,14 +340,15 @@ void loop() {
       Serial.println("Change color btn pressed");
     }
 
-    if (++current_color >= collors_count) {
-      current_color = 0;
-    }
+    // check if animation switch off and then change color
+    if (!animation_state) {
+      changeCurrentColor();
 
-    fill_solid(leds, 256, collors[current_color]);
-    FastLED.show();
+      // start animation
+      animation_state = HIGH;
 
-    updateEEPROM();
+      updateEEPROM();
+    }    
   }
 
   btn_change_color_last_state = btn_change_color_current_state;
@@ -219,15 +364,23 @@ void loop() {
       Serial.println("Switch names btn pressed");
     }
 
-    if (++current_color >= collors_count) {
-      current_color = 0;
-    }
-
-    fill_solid(leds, 256, collors[current_color]);
-    FastLED.show();
-
     updateEEPROM();
   }
 
   btn_switch_names_last_state = btn_switch_names_current_state;
+
+  //check animation
+  if (animation_state) {
+    //if (millis() - global_timer > GLOBAL_LOOP) {
+      global_timer = millis();  // reset main timer
+
+      if (millis() - mode_timer > COLOR_MODE_SPEED) {
+        mode_timer = millis();  // reset mode timer
+
+        animateSwitchOn();
+      }
+    //}
+
+    FastLED.show();
+  }
 }
